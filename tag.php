c6842d24a8d58c3863a96305f2fcde38513dc0f6
Veronika Hanzlíková <hanzlikova@shoptet.cz>
2018-08-03 12:03:51 +0200

    tag page

--- tag.php
+++ tag.php
@@ -2,7 +2,11 @@
 
 <section class="section section-perex">
     <div class="section-inner container">
+        <?php get_template_part( 'template-parts/utils/content', 'breadcrumb' ); ?>
+
         <h1>Tag: <?php single_tag_title(); ?></h1>
+
+        <?php echo tag_description(); ?>
     </div>
 </section>
 
