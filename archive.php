4c2b10799a572c5ceb17c89403653f6707f51383
Veronika Hanzlíková <hanzlikova@shoptet.cz>
2018-08-24 15:45:20 +0200

    Archive.php added for unsupported archive types (e.g. date)

---
+++ archive.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<section class="section section-perex">
+    <div class="section-inner container">
+        <?php get_template_part( 'template-parts/utils/content', 'breadcrumb' ); ?>
+        <h1><?php the_archive_title(); ?></h1>
+        <?php echo the_archive_description(); ?>
+    </div>
+</section>
+
+<?php get_template_part( 'template-parts/post/content', 'section-primary' ); ?>
+
+<?php get_footer(); ?>
